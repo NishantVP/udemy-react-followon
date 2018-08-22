d5e72e9408dc1fc5948cfe128b055dbd663a64cd
Nishant Phatangare <you@example.com>
2018-08-21 23:54:05 -0700

    lecture 45

--- src/App.js
+++ src/App.js
@@ -11,22 +11,39 @@ class App extends Component {
         otherState : "random"
     };
 
-    switchNameHandler = () => {
-        // console.log("was clicked")
+    switchNameHandler = (newName) => {
         this.setState( {
             persons: [
-                {name: 'Nishant', age: 27},
+                {name: newName, age: 27},
                 {name: 'Daenerys Targaryen', age: 22}
                 ]
+        })
+    };
+
+    changeNameHandler = (event) => {
+        this.setState( {
+            persons: [
+                {name: event.target.value, age: 27},
+                {name: 'Daenerys Targaryen', age: 22}
+            ]
         })
     };
 
     render() {
         return (
             <div className="App">
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} > I am the king in the north.</Person>
-                <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} > I am the rightful hair to the iron thrown.</Person>
+                <button onClick={() => this.switchNameHandler('Nishant')}>Switch Name</button>
+                <Person
+                    name = {this.state.persons[0].name}
+                    age = {this.state.persons[0].age}
+                    click = {this.switchNameHandler.bind(this, 'Rob')}
+                    changed = {this.changeNameHandler}
+                > I am the king in the north. </Person>
+                <Person
+                    name = {this.state.persons[1].name}
+                    age = {this.state.persons[1].age}
+                    click = {this.switchNameHandler.bind(this, 'Ned')}
+                > I am the rightful hair to the iron thrown. </Person>
             </div>
         );
     }
